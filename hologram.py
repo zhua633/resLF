94a3a91294d2463c0e8488d0bf066575a9b7e829
zhua633 <zhua633@aucklanduni.ac.nz>
2022-07-26 16:33:40 +1200

    Hologram works

---
+++ hologram.py
@@ -0,0 +1,69 @@
+# Importing libraries
+import numpy as np
+import math
+import cv2
+from PIL import Image    
+import statistics
+
+filepath=r"C:\Users\Anvilly Huang\Documents\GitHub\Superscaling\test_images\0609"
+
+
+# Number of pixels
+S=512 
+I=np.zeros((S,S),dtype=np.float64)
+
+# Implement a function that randomises the size (width 1-2 pixels, length 3-5 pixels), rotation and relative position of the object of the centre by +- 10%
+# Size at the moment: 1x4
+for row in range(254,256):
+    for col in range(254,258):
+        I[row, col]=1
+
+I=np.float64(I)
+
+# Parameter setup
+M = len(I)
+deltax=3.7*10**(-6) #pixel pitch 0.001 cm (10 um)
+w=633*10**(-9); # wavelength 633 nm
+k0 = 2*math.pi/w
+
+# implement a function that adjust this value by +- 20%
+z=0.005; #25 cm, propagation distance
+
+# Step 1: simulation of propagation using the ASM
+deltaf=1/M/deltax
+
+x = list(range(1, M+1))
+C, R= np.meshgrid(x, x)
+
+# FORWARD PROPAGATION USING ASM
+Ref = 50*math.sqrt(np.mean(I))*np.exp(-1j*k0*z) #Reference wave 
+A01=np.fft.fftshift(np.fft.ifft2(np.fft.fftshift(I))); #Spectrum first object
+
+# dproduct1=np.dot((R-M/2-1),deltaf)
+# dproduct2=np.dot(dproduct1,dproduct1)
+# dproduct3=np.dot((C-M/2-1),deltaf)
+# dproduct4=np.dot(dproduct3,dproduct3)
+
+# p1=np.exp(np.dot(-2j*math.pi*z,np.sqrt((1/w)**2-dproduct2-dproduct4))); #Propagation factor in the spectrum domain z1
+p1=np.exp(-2j*math.pi*z*np.sqrt((1/w)**2-((R-M/2-1)*deltaf)**2-((C-M/2-1)*deltaf)**2))
+
+Az1=A01*p1#Propagating
+print(Az1)
+
+H1 = np.fft.fftshift(np.fft.fft2(np.fft.fftshift(Az1)))
+
+# H1 = np.dot((abs(H1 + Ref)),(abs(H1 + Ref)))
+H1 = (abs(H1 + Ref))**2
+
+H1_=abs(H1)-np.amin(abs(H1))
+img1 = Image.fromarray(np.uint8((abs(H1_)/np.amax(abs(H1_))) * 255),'L')
+img1.save(f"{filepath}/H1.png")
+
+width,height=H1.shape
+img1.thumbnail((width/2,height/2), Image.ANTIALIAS)
+img1.show()
+img1.save(f"{filepath}/H2.png")
+
+
+
+
